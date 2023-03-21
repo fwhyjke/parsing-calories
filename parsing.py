a482b57065d97440df83b38bb60bed268a7ee60e
fwhy <timofeevila423@gmail.com>
2023-03-21 08:01:06 +0700

    Translated products into English and added process tracking.

--- parsing.py
+++ parsing.py
@@ -14,10 +14,16 @@ def parsing() -> list[list[str, str, str, str, str], ...]:
     links_to_eat = {i.find('span').text: f'https://health-diet.ru{i.get("href")}' for i in
                     soup.find_all('a', class_='mzr-left-menu-item uk-link-reset')}
 
+    quantity_of_links = len(links_to_eat)
+    counter = 0
+
     for url in links_to_eat:
         req = requests.get(links_to_eat[url], headers=headers).text
         soup = BeautifulSoup(req, 'lxml')
         eat_info_html = [[url] + [j.text.strip() for j in i.find_all('td')] for i in soup.find('tbody').find_all('tr')]
         data.extend(eat_info_html)
+        counter += 1
+        print(f'Processing... {counter} out of {quantity_of_links}')
 
+    print('Done ')
     return data
